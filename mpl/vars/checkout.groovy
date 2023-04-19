9d73117e5a77b6ce32d9e8fd6d3b5006b2476032
Nelson Ruiz <nard8102@hotmail.com>
2023-04-19 17:59:57 -0500

    delete

--- mpl/vars/checkout.groovy
+++
@@ -1,9 +0,0 @@
-// vars/checkout.groovy
-def call(Map configMap) {
-    def scm = configMap['scm']
-    checkout([
-        $class: 'GitSCM',
-        branches: scm.branches,
-        userRemoteConfigs: scm.userRemoteConfigs,
-    ])
-}
--- mpl/vars/k8s_variables.groovy
+++
@@ -1,3 +0,0 @@
-def url = 'https://github.com/naiveskill/devops.git'
-def branch = 'main'
-def credentialsId = 'nard8102@hotmail.com'
