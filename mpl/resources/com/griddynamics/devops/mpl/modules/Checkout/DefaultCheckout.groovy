565f5339bc606bd60cc083d9f7800b032d5f6196
Nelson Ruiz <nard8102@hotmail.com>
2023-04-19 19:26:51 -0500

    change ${WORKSPACE}

--- mpl/resources/com/griddynamics/devops/mpl/modules/Checkout/DefaultCheckout.groovy
+++ mpl/resources/com/griddynamics/devops/mpl/modules/Checkout/DefaultCheckout.groovy
@@ -6,6 +6,7 @@ checkout scm
 @Library('mpl@main') _
 
 node {
-    def checkoutModule = load "checkout.groovy"
+    // def checkoutModule = load "checkout.groovy"
+    def checkoutModule = load "${WORKSPACE}/vars/checkout.groovy"
     checkoutModule.call(url: 'https://github.com/naiveskill/devops.git', branch: 'main')
 }
