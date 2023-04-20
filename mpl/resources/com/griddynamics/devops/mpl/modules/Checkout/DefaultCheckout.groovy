20a3dddb5c980514754e7e60cb54da5c243e8e50
Nelson Ruiz <nard8102@hotmail.com>
2023-04-19 20:16:35 -0500

    checkouyt

--- mpl/resources/com/griddynamics/devops/mpl/modules/Checkout/DefaultCheckout.groovy
+++ mpl/resources/com/griddynamics/devops/mpl/modules/Checkout/DefaultCheckout.groovy
@@ -3,7 +3,7 @@
 
 checkout scm
 */
-@Library('mpl@main')
+/*@Library('mpl@main')
 
 def branch = 'main'
 def url = 'https://github.com/naiveskill/devops.git'
@@ -11,4 +11,16 @@ def url = 'https://github.com/naiveskill/devops.git'
 node {
     def checkoutModule = load "scmcheckout.groovy"
     checkoutModule.call(url: url, branch: branch)
+}
+*/
+
+def branch = 'main'
+def url = 'https://github.com/naiveskill/devops.git'
+
+def call(String url, String branch) {
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: "*/${branch}"]],
+        userRemoteConfigs: [[url: "${url}"]]
+    ])
 }
