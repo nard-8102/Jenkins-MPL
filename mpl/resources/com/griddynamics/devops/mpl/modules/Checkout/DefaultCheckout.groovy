1bc8dc4df412c0d02c8b26cfd4b654e8b8490090
Nelson Ruiz <nard8102@hotmail.com>
2023-04-19 19:41:53 -0500

    change checkout

--- mpl/resources/com/griddynamics/devops/mpl/modules/Checkout/DefaultCheckout.groovy
+++ mpl/resources/com/griddynamics/devops/mpl/modules/Checkout/DefaultCheckout.groovy
@@ -8,12 +8,6 @@ def branch = 'main'
 def url = 'https://github.com/naiveskill/devops.git'
 
 node {
-    // def checkoutModule = load "checkout.groovy"
-    // def checkoutModule = load "${WORKSPACE}/vars/checkout.groovy"
-    // checkoutModule.call(url: 'https://github.com/naiveskill/devops.git', branch: 'main')
-    checkout([
-        $class: 'GitSCM',
-        branches: [[name: "*/${branch}"]],
-        userRemoteConfigs: [[url: "${url}"]]
-    ])
+    def checkoutModule = load "checkout.groovy"
+    checkoutModule.call(url: url, branch: branch)
 }
