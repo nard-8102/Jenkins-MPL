ead2f59a8d6cdf500691384a111a3fa39e5e824e
Nelson Ruiz <nard8102@hotmail.com>
2023-04-19 21:15:45 -0500

    change

--- mpl/vars/scmcheckout.groovy
+++ mpl/vars/scmcheckout.groovy
@@ -1,7 +1,9 @@
-def branch = 'main'
-def url = 'https://github.com/naiveskill/devops.git'
+
 
 def call(String url, String branch) {
+    def branch = 'main'
+    def url = 'https://github.com/naiveskill/devops.git'
+
     checkout([
         $class: 'GitSCM',
         branches: [[name: "*/${branch}"]],
